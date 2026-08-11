12432449f5a495bdbf07ab6739eae210774711af
Kevin Johnson <kevin.johnson@viam.com>
2026-08-11 11:14:23 -0400

    avoid material import

--- example/lib/screens/widgets/input_screen.dart
+++ example/lib/screens/widgets/input_screen.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 import 'package:prime_flutter/prime_flutter.dart';
 
 class InputScreen extends StatelessWidget {
